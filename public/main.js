7897a773f7becc10a442bc1be061be34f9be17a7
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-12-14 11:30:43 +0100

    updated fetch url

--- public/main.js
+++ public/main.js
@@ -2,7 +2,7 @@ document.querySelector("#result").addEventListener("click", makeReq);
 
 async function makeReq() {
   // const userName = document.querySelector("#username").value;
-  const res = await fetch(`https://wide-eyed-pear-costume.cyclic.app/api/`);
+  const res = await fetch(`https://coinflip.cyclic.app/api/`);
   console.log(res);
   const data = await res.json();
   console.log(data);
