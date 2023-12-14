af45e430c8f0a2a64e20e66931b3760b57d022a0
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-12-14 11:35:49 +0100

    fixed DOM error

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
+const cors = require("cors");
+
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.static("public"));
 
